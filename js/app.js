56372f5b74a22c657fd46e87958cde6072af0c48
Armin Taheri <armin.ytaheri@gmail.com>
2015-09-27 19:47:16 +0000

    third commit

--- js/app.js
+++ js/app.js
@@ -21,36 +21,32 @@ function Application () {
         document.body.appendChild(renderer.domElement)
         
         wavemap = WaveMap();
-		
-		/* waveMat = new THREE.ShaderMaterial({
-		    uniforms: THREE.UniformsUtils.merge([
-                THREE.UniformsLib['lights'],
-                {
-                    color: {type: 'f', value: 1.0},
-                    heightMul: {type: 'f', value: 1.0},
-                    heightmap: {type: 't', value: wavemap.getNextWaveMap()}
-                }
-            ]),
-            vertexShader: document.
-                          getElementById('waveVert').text,
-            fragmentShader: document.
-                            getElementById('waveFrag').text,
-            transparent: true,
-            lights: true
-		}); */
-		
-		wavePlane = new THREE.Mesh(new THREE.PlaneGeometry(6,6,64,64), wavemap.getNextWaveMap());
-		//wavePlane.rotation.y = -Math.PI;
-		//wavePlane.rotation.x = -Math.PI/2;
-		scene.add(wavePlane);
-
+        
+        /* waveMat = new THREE.ShaderMaterial({
+        uniforms: THREE.UniformsUtils.merge([
+            THREE.UniformsLib['lights'],
+            {
+                color: {type: 'f', value: 1.0},
+                heightMul: {type: 'f', value: 1.0},
+                heightmap: {type: 't', value: wavemap.getNextWaveMap()}
+            }
+        ]),
+        vertexShader: document.getElementById('waveVert').text,
+        fragmentShader: document.getElementById('waveFrag').text,
+        transparent: true,
+        lights: true
+        }); */
+        
+        wavePlane = new THREE.Mesh(new THREE.PlaneGeometry(6,6,64,64), wavemap.getWaveMaterial());
+        scene.add(wavePlane);
+        
         light = new THREE.PointLight(0xffffff, 1, 100);
         light.position.set(0,2,-1);
         scene.add(light);
-		
-		camera.position.z = 6;
-		//camera.position.y = 5;
-		//camera.rotation.x = -0.7;
+        
+        camera.position.z = 6;
+        //camera.position.y = 5;
+        //camera.rotation.x = -0.7;
     };
 	
 	var render = function () {
